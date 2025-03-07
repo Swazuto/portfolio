1bc47d7a62ffd70d43751f8cd4744c907edf96d3
Swazuto <undeadgames3@gmail.com>
2025-03-07 13:17:50 -0300

    :sparkles: feat: New Servers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,9 +69,9 @@ const Where = () => {
           what="Um servidor que criei para obter conhecimento, mas que com divulgações e colaborações, se mantém até hoje."
         />
         <Co
-          url="https://discord.gg/gzXBwGjD8P"
+          url="https://discord.gg/newdev"
           name="New Dev"
-          iconReference='https://i.imgur.com/uIRn5vH.png'
+          iconReference='https://cdn.discordapp.com/icons/758308791837786232/5c6ff8778d1b6bb82e349b3be490f5fd.webp'
           tagline="Servidor de Discord"
           role="Fundador & Desenvolvedor"
           pretext="Loja"
@@ -80,7 +80,7 @@ const Where = () => {
         <Co
           url="https://discord.gg/elhero-gg"
           name="El Hero"
-          iconReference='https://cdn.discordapp.com/icons/1232448776917221427/3a9d517a0ea2bf2e5712b22ccce55c59.png'
+          iconReference='https://cdn.discordapp.com/icons/1232448776917221427/4e8bdbcbaa0735253dfecdc0a0f3e122.webp'
           tagline="Servidor de Discord"
           role="Administrador & C.M."
           pretext="Comunidade"
@@ -93,7 +93,7 @@ const Where = () => {
           tagline="Servidor de Discord"
           role="Administrador"
           pretext="Comunidade"
-          what="Discord focado em fazer dinheiro na Web3, Salve ABC.eth!"
+          what="Discord focado em fazer dinheiro na Web3, Salve ABC!"
         />
         <Co
           url="https://discord.gg/8gE88envWq"
@@ -107,29 +107,20 @@ const Where = () => {
         <Co
           url="https://squarecloud.app/"
           name="Square Cloud"
-          iconReference='https://cdn.discordapp.com/icons/898377990906454016/aec69d4445afae814820085e69b6a795.webp'
+          iconReference='https://cdn.discordapp.com/emojis/1323647179516280875.png'
           tagline="Site de Hospedagem"
-          role="Community Helper"
+          role="Ex-Community Helper"
           pretext="Loja"
           what="Uma empresa de hospedagem com o intuito direto de ajudar experts e noobs a hospedarem de forma fácil e rápida os seus bots, sites e APIs."
         />
         <Co
-          url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-          name="Cake de Morango"
-          iconReference='https://media.tenor.com/t4zUkk1R-UIAAAAe/pica-pau.png'
-          tagline="Servidor de Amigos"
-          role="Amigo"
-          pretext="Amigos"
-          what="Um servidor de amigos, praticamente todos os meus amigos estão aqui, se você não está aqui, provavelmente não somos conhecidos o bastante."
-        />
-        <Co
-          url="https://discord.gg/XjupgHw3Fj"
-          name="Godly 神 | Suporte (Insider)"
-          iconReference='https://cdn.discordapp.com/icons/987804101163233331/4dd8ba2e99f3d2fe6da708cb4f0077c3.webp'
-          tagline="Servidor de Suporte"
-          role="Fundador & Desenvolvedor"
+          url="https://discord.gg/JkRfF3CtHb"
+          name="Clipada"
+          iconReference='https://cdn.discordapp.com/icons/1274068331695181928/8ae4642db82580529c5f3c0cd86e5d8a.webp'
+          tagline="Servidor de Discord"
+          role="Desenvolvedor & C.M."
           pretext="Suporte"
-          what="Um servidor focado em resolver problemas e postar atualizações de projetos grandes e as vezes gratuitos."
+          what="Discord do site clipada.com, criado por El Gato, com intuito de te gerar dinheiro com base no seu engajamento"
         />
         <Co
           url="https://discord.gg/radiantz"
@@ -138,7 +129,34 @@ const Where = () => {
           tagline="Servidor de Discord"
           role="Fundador & Desenvolvedor"
           pretext="Comunidade"
-          what="Discord focado em fazer dinheiro na Web3, Salve Kaio!"
+          what="Discord focado em fazer dinheiro na Web3, Salve FireZy!"
+        />
+        <Co
+          url="https://discord.gg/thehorderon"
+          name="The Horde"
+          iconReference='https://cdn.discordapp.com/icons/1302689489759502347/a_362287fbeb773f8fccc40719419041ac.gif'
+          tagline="Coleção de NFTs"
+          role="Desenvolvedor"
+          pretext="Comunidade"
+          what="Discord da coleção de NFTs da rede Ronin."
+        />
+        <Co
+          url="https://www.exospirits.com/"
+          name="ExoSpirits"
+          iconReference='https://pbs.twimg.com/profile_images/1872710135342141440/nCLGyIVX_400x400.jpg'
+          tagline="Coleção de NFTs"
+          role="Desenvolvedor"
+          pretext="Comunidade"
+          what="Discord da coleção de NFTs da rede Monad."
+        />
+        <Co
+          url="https://www.blinkplanet.com/"
+          name="BlinkPlanet"
+          iconReference='https://cdn.discordapp.com/icons/1133021036246478858/8478b17c9f63644e8557a2412a6a0245.webp'
+          tagline="Coleção de NFTs"
+          role="Desenvolvedor"
+          pretext="Comunidade"
+          what="Discord da coleção de NFTs da rede Polygon."
         />
       </div>
     </PageWrapper>
